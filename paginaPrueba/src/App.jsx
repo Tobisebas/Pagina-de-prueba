e785e91500c7d6d94620f97dc183646150d0b6f1
Tobias Abregu <a20213174@pucp.edu.pe>
2023-10-28 13:04:23 -0500

    nueva pagina

---
+++ paginaPrueba/src/App.jsx
@@ -0,0 +1,58 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <div>
+          InterNoticias
+        </div>
+      </header>
+      <main>
+        <h2>Bienvenido a InterNoticias, donde podras ver las noticias mas recientes y controversiales</h2>
+        <h1>Noticia hipotetica</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, quibusdam quos, amet sed quod explicabo ipsum, vero unde corrupti nulla laudantium error? Itaque animi sapiente optio eaque quasi doloribus tempora.
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur praesentium aliquam blanditiis illo eligendi maxime quos expedita perferendis, nemo saepe asperiores odio voluptatum sit hic, harum impedit incidunt ratione! Aliquam!
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint, harum. Atque dolor dolorum, aliquid adipisci, reiciendis distinctio commodi delectus aspernatur tempora quasi quibusdam architecto provident sint saepe earum? Quae, distinctio.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam cum odio nihil, maxime debitis unde qui pariatur eveniet sint voluptatum praesentium necessitatibus doloremque veniam explicabo eligendi quod, animi mollitia accusamus?
+        </p>
+        <div className='primera_parte'>
+          <img className='imagen_primaria' src="" alt="" height="300px" width="300px" />
+        </div>
+        <h1>Sobre la noticia...</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsam ullam magnam a nisi porro nam, consequatur voluptatum eum veniam alias impedit debitis qui quia eos quas pariatur deserunt numquam?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. At eaque a quos sint possimus sequi, quas atque aut laboriosam ratione, odit fugit omnis blanditiis cum deleniti, alias repudiandae sapiente quaerat?
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod dolores, animi minima magnam corrupti ducimus. Aperiam dolore iure modi fugit rem, officia maxime nesciunt minus voluptatem numquam ut, adipisci quia
+        </p>
+        <div className='imagenes_secundarias'>
+          <img src="" alt="" height="300px" width="300px" />
+          <img src="" alt="" height="300px" width="300px" />
+        </div>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia quos dicta laboriosam quibusdam itaque nihil ipsam, iusto minus vel dolores autem suscipit quas deleniti ad minima eius mollitia! Harum, eligendi?
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis a quibusdam eligendi iusto officia, soluta laboriosam sunt aperiam est error quidem quia neque. Autem doloremque in perferendis, eius aliquid explicabo?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim magnam molestiae, odio dolores qui aliquid porro cum fugiat harum quo necessitatibus laudantium recusandae eius esse ut dolorem optio asperiores cumque?
+        </p>
+        <img className='imagen_grande' src="" alt="" height="300px" width="100%" />
+
+        <h1>Conclusiones!</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci tempore minima dolor eius provident, ex aliquid quidem pariatur fugiat id quibusdam illum quasi cupiditate laboriosam enim. Debitis, nesciunt eum. Rem?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse omnis maiores voluptas delectus, totam iste libero quidem ipsa quo qui maxime culpa, consequatur dolor. Iste quod voluptate quos eveniet quibusdam!
+        </p>
+      </main>
+      <footer>
+        <span>Created 2023 - Anonimo</span>
+        <div>
+          <a href="">Facebook</a><a href="">Instagram</a><a href="">X</a>
+        </div>
+      </footer>
+
+    </>
+  )
+}
+
+export default App
